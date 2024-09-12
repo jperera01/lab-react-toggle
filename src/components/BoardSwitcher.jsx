5b84caded5c010dc48dd9f1c0e77c61361dd78a5
jperera01 <thebluewave2@yahoo.com>
2024-09-11 20:35:50 -0400

    finished lab functionality

--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +10,31 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  //useState to manage the selected board index
+  const [selectedBoardIndex, setSelectedBoardIndex] = useState(0);
+
+  // handles toggling the selected board
+  const handleToggle = () => {
+    // Increment the selected board index, wrapping around to 0 when reaching the total number of boards.
+
+    setSelectedBoardIndex((prevIndex) => (prevIndex + 1) % props.numBoards);
+  };
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    // Check if the current board should be selected
+    let isSelected = ii === selectedBoardIndex;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      {/*add the onClick event handler to the button and display current selection */}
+      <button onClick={handleToggle}>Toggle</button>
+      <span style={{ marginLeft: "10px" }}>
+        current box: {selectedBoardIndex + 1}
+      </span>
     </div>
   );
 }
